5bc49fd83b958b16ff8a2e3f10114a7931a2c312
Efstathios Iosifidis <iefstathios@gmail.com>
2021-04-19 23:00:17 +0300

    Assignments Trees

---
+++ s2/data_structures/f5/a11f5.c
@@ -0,0 +1,205 @@
+/*  Αρχείο: a11f5.c
+    Φοιτητής: Ευστάθιος Ιωσηφίδης
+    ΑΜ: iis21027
+
+    Άδεια χρήσης: GNU General Public License v3.0
+    
+    Γράψτε ένα πρόγραμμα που θα πραγματοποιεί ορθογραφικό έλεγχο ενός κειμένου. Οι λέξεις που απαρτίζουν το λεξικό
+    είναι αποθηκευμένες στο αρχείο κειμένου ‘I112f5.TXT’ (σε κάθε γραμμή του υπάρχει μία λέξη). Διαβάζονται και
+    αποθηκεύονται μία-μία σε ένα ΔΔΑ με τη συνάρτηση CreateDictionary και έτσι δημιουργείται το λεξικό-ΔΔΑ. Μετά τη
+    δημιουργία του λεξικού-ΔΔΑ θα εμφανίζετε τις λέξεις του λεξικού. Στη συνέχεια το πρόγραμμα μέσω της συνάρτησης
+    SpellingCheck θα διαβάζει ένα κείμενο που είναι αποθηκευμένο στο αρχείο κειμένου ‘I111F5.TXT’, σε κάθε γραμμή του
+    οποίου υπάρχει μία λέξη και θα διενεργεί ορθογραφικό έλεγχο, δηλαδή θα αναζητά τη λέξη του αρχείου ‘I111F5.TXT’ στο
+    λεξικό-ΔΔΑ. Κατά τον ορθογραφικό έλεγχο θα πρέπει να εκτυπώνετε τις λέξεις που δεν βρέθηκαν στο λεξικό και να
+    υπολογίζετε το πλήθος τους. Το πλήθος των λέξεων που δεν βρέθηκαν στο λεξικό θα εμφανίζεται στη main.
+    Δίνονται τα πρωτότυπα των συναρτήσεων
+    void CreateDictionary(BinTreePointer *Root, FILE *fp);
+    int SpellingCheck(BinTreePointer Root, FILE *fp);
+    
+    Η 1 παράμετρος της fopen θα πρέπει να περιλαμβάνει το όνομα του αρχείου χωρίς διαδρομή δηλαδή:
+    fp1 = fopen("i112f5.txt", "r");
+    fp2 = fopen("i111f5.txt", "r");
+    
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define size 20
+
+// Δήλωση τύπων
+typedef char BinTreeElementType[size];
+
+typedef struct BinTreeNode *BinTreePointer;
+struct BinTreeNode {
+    BinTreeElementType Data;
+    BinTreePointer LChild, RChild;
+} ;
+
+typedef enum {
+    FALSE, TRUE
+} boolean;
+
+// Δήλωση συναρτήσεων
+void CreateBST(BinTreePointer *Root);
+boolean EmptyBST(BinTreePointer Root);
+void BSTInsert(BinTreePointer *Root, BinTreeElementType Item);
+void BSTSearch(BinTreePointer Root, BinTreeElementType KeyValue, boolean *Found, BinTreePointer *LocPtr);
+
+void InorderTraversal(BinTreePointer Root);
+
+void CreateDictionary(BinTreePointer *Root, FILE *fp);
+int SpellingCheck(BinTreePointer Root, FILE *fp);
+
+int main () 
+{
+    BinTreePointer ARoot;
+
+    FILE *fp1;
+    FILE *fp2;
+
+    fp1 = fopen("i112f5.txt","r");
+    fp2 = fopen("i111f5.txt","r");
+
+    CreateBST(&ARoot);
+    CreateDictionary(&ARoot,fp1);
+
+    printf("**********Dictionary**********\n");
+    InorderTraversal(ARoot);
+    printf("\n");
+
+
+    printf("**********Not in Dictionary**********\n");
+    printf("Number of words not in Dictionary: %d\n",SpellingCheck(ARoot, fp2));
+
+    fclose(fp1);
+    fclose(fp2);
+
+    return 0;
+}
+
+
+// Υλοποίηση συναρτήσεων
+void CreateBST(BinTreePointer *Root)
+/* Λειτουργία: Δημιουργεί ένα κενό ΔΔΑ.
+   Επιστρέφει:  Τον μηδενικό δείκτη(NULL) Root
+*/
+{
+    *Root = NULL;
+}
+
+boolean EmptyBST(BinTreePointer Root)
+/* Δέχεται:   Ενα ΔΔα με το Root να δείχνει στη ρίζα του.
+  Λειτουργία: Ελέγχει αν το ΔΔΑ είναι κενό.
+  Επιστρέφει: TRUE αν το ΔΔΑ είναι κενό και FALSE διαφορετικά
+*/
+{   
+    return (Root==NULL);
+}
+
+void BSTInsert(BinTreePointer *Root, BinTreeElementType Item)
+/* Δέχεται:     Ένα ΔΔΑ με το δείκτη Root να δείχνει στη ρίζα του και ένα στοιχείο Item.
+   Λειτουργία:  Εισάγει το στοιχείο Item στο ΔΔΑ.
+   Επιστρέφει: Το τροποποιημένο ΔΔΑ με τον δείκτη Root να δείχνει στη ρίζα του
+*/
+{
+    BinTreePointer LocPtr, Parent;
+    boolean Found;
+
+    LocPtr = *Root;
+    Parent = NULL;
+    Found = FALSE;
+    while (!Found && LocPtr != NULL) {
+        Parent = LocPtr;
+        if (strcmp(Item, LocPtr->Data) < 0)
+            LocPtr = LocPtr ->LChild;
+        else if (strcmp(Item, LocPtr->Data) > 0)
+            LocPtr = LocPtr ->RChild;
+        else
+            Found = TRUE;
+    }
+    if (Found)
+        printf("To %s EINAI HDH STO DDA\n", Item);
+    else {
+        LocPtr = (BinTreePointer)malloc(sizeof (struct BinTreeNode));
+        //LocPtr ->Data = Item;//kati edo
+        strcpy(LocPtr ->Data,Item);
+        LocPtr ->LChild = NULL;
+        LocPtr ->RChild = NULL;
+        if (Parent == NULL)
+            *Root = LocPtr;
+        else if (strcmp(Item,Parent ->Data) < 0)
+            Parent ->LChild = LocPtr;
+        else
+            Parent ->RChild = LocPtr;
+    }
+}
+
+void BSTSearch(BinTreePointer Root, BinTreeElementType KeyValue, boolean *Found, BinTreePointer *LocPtr)
+/* Δέχεται:    Ένα ΔΔΑ με το δείκτη Root να δείχνει στη ρίζα του και μια τιμή KeyValue.
+   Λειτουργία: Αναζητά στο ΔΔΑ έναν κόμβο με τιμή KeyValue στο πεδίο κλειδί του.
+   Επιστρέφει: Η Found έχει τιμή TRUE και ο δείκτης LocPtr δείχνει στον κόμβο που
+                περιέχει την τιμή KeyValue, αν η αναζήτηση είναι επιτυχής.
+                Διαφορετικά η Found έχει τιμή FALSE
+*/
+{
+    (*LocPtr) = Root;
+    (*Found) = FALSE;
+    while (!(*Found) && (*LocPtr) != NULL)
+    {
+        if (strcmp(KeyValue,(*LocPtr)->Data) < 0)
+            (*LocPtr) = (*LocPtr)->LChild;
+        else
+            if (strcmp(KeyValue,(*LocPtr)->Data) > 0)
+                (*LocPtr) = (*LocPtr)->RChild;
+            else (*Found) = TRUE;
+    }
+}
+
+void InorderTraversal(BinTreePointer Root)
+/* Δέχεται:    Ένα δυαδικό δέντρο με το δείκτη Root να δείχνει στην ρίζα του.
+   Λειτουργία: Εκτελεί ενδοδιατεταγμένη διάσχιση του δυαδικού δέντρου και
+                επεξεργάζεται κάθε κόμβο ακριβώς μια φορά.
+   Εμφανίζει: Το περιεχόμενο του κόμβου, και εξαρτάται από το είδος της επεξεργασίας
+*/
+{
+    if (Root!=NULL) {
+        InorderTraversal(Root->LChild);
+        printf("%s\n",Root->Data);
+        InorderTraversal(Root->RChild);
+    }
+}
+
+void CreateDictionary(BinTreePointer *Root, FILE *fp)
+{
+    BinTreeElementType Item;
+
+    while(!feof(fp)){
+
+        fscanf(fp,"%s", Item);
+        BSTInsert(&(*Root), Item);
+    }
+}
+
+int SpellingCheck(BinTreePointer Root, FILE *fp)
+{
+    boolean Found;
+    BinTreePointer LocPtr;
+    BinTreeElementType Item;
+    int count = 0;
+
+    while(!feof(fp)){
+
+        fscanf(fp,"%s", Item);
+        BSTSearch(Root, Item, &Found, &LocPtr);
+
+        if(!Found){
+            printf("%s \n",Item);
+            count++;
+        }
+    }
+
+    return count;
+}
---
+++ s2/data_structures/f5/a29f5.c
@@ -0,0 +1,223 @@
+/*  Αρχείο: a29f5.c
+    Φοιτητής: Ευστάθιος Ιωσηφίδης
+    ΑΜ: iis21027
+
+    Άδεια χρήσης: GNU General Public License v3.0
+    
+    Γράψτε πρόγραμμα που θα δέχεται για κάθε άτομο τον ΑΜΚΑ (ακέραιος), τον ΑΦΜ (ακέραιος), την ηλικία (ακέραιος). Θα
+    καταχωρεί τα στοιχεία του κάθε ατόμου σε 2 καταλόγους ανάλογα με την ηλικία του, αυτούς με ηλικία μικρότερη ή ίση
+    των 60 ετών και αυτούς με ηλικία μεγαλύτερη των 60.
+    Κάθε κατάλογος θα πρέπει να οργανωθεί ως ΔΔΑ με κλειδί τον ΑΜΚΑ. Το πρόγραμμα θα περιλαμβάνει τις εξής
+    λειτουργίες
+    - Εισαγωγή των στοιχείων του κάθε ατόμου στο αντίστοιχο ΔΔΑ ανάλογα με την ηλικία του (ΔΔΑ για άτομα με ηλικίες
+    <=60 και ΔΔΑ για άτομα με ηλικίες > 60). Και στα 2 ΔΔΑ το κλειδί θα είναι ο ΑΜΚΑ.
+    - Εμφάνιση των 2 καταλόγων
+    - Αναζήτηση ατόμου με βάση τον ΑΜΚΑ και την ηλικία.
+    Δίνεται ένα στιγμιότυπο εκτέλεσης όπου φαίνεται πως θα γίνεται το διάβασμα των δεδομένων και η εμφάνιση του
+    κάθε καταλόγου. Η αναζήτηση θα γίνει για 3 άτομα: α) ένα άτομο του οποίου τα στοιχεία έχουν καταχωρηθεί σε ένα εκ
+    των 2 καταλόγων (ΑΜΚΑ, ηλικία), β) ένα άτομο του οποίου το ΑΜΚΑ του έχει καταχωρηθεί στον αντίστοιχο κατάλογο
+    αλλά η ηλικία του δεν ταυτίζεται με τη δοθείσα και γ) ένα άτομο του οποίου το ΑΜΚΑ δεν έχει καταχωρηθεί σε κανένα
+    κατάλογο. Τα μηνύματα της κάθε περίπτωσης φαίνονται στο στιγμιότυπο εκτέλεσης.
+    (για την απλοποίηση της εισαγωγής δεδομένων δόθηκαν ίδιες τιμές για ΑΜΚΑ, ΑΦΜ, ηλικία)
+    
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+// Δήλωση τύπων
+typedef struct{
+    int AMKA;
+    int afm;
+    int age;
+} BinTreeElementType;
+
+typedef struct BinTreeNode *BinTreePointer;
+ struct BinTreeNode {
+    BinTreeElementType Data;
+    BinTreePointer LChild, RChild;
+} ;
+
+typedef enum {
+    FALSE, TRUE
+} boolean;
+
+// Δήλωση συναρτήσεων
+void CreateBST(BinTreePointer *Root);
+boolean EmptyBST(BinTreePointer Root);
+void BSTInsert(BinTreePointer *Root, BinTreeElementType Item);
+void BSTSearch(BinTreePointer Root, BinTreeElementType KeyValue, boolean *Found, BinTreePointer *LocPtr);
+void InorderTraversal(BinTreePointer Root);
+
+int main()
+{
+    BinTreePointer Root1, Root2, LocPtr;
+    BinTreeElementType person;
+    boolean found;
+    char ch;
+    int i;
+
+    CreateBST(&Root1);
+    CreateBST(&Root2);
+
+    do
+    {
+     printf("Give AMKA? ");
+     scanf("%d", &person.AMKA);
+     printf("Give AFM? ");
+     scanf("%d", &person.afm);
+     printf("Give age? ");
+     scanf("%d", &person.age);
+    
+     if(person.AMKA<=60){
+        BSTInsert(&Root1, person);
+     }
+     else{
+        BSTInsert(&Root2, person);
+     }
+     printf("\nContinue Y/N: ");
+     
+     do
+       {
+       scanf("%c", &ch);
+       } while (toupper(ch)!= 'N' && toupper(ch)!= 'Y');
+       
+       } while (toupper(ch)!='N');
+
+    printf("\nPeople with age less-equal 60\n");
+    InorderTraversal(Root1);
+    printf("\n");
+    printf("\nPeople with age greater than 60\n");
+    InorderTraversal(Root2);
+    printf("\n");
+    for(i=0;i<3;i++)
+    {
+     printf("Give AMKA: ");
+     scanf("%d", &person.AMKA);
+     printf("Give age: ");
+     scanf("%d", &person.age);
+
+    if(person.age<=60)
+    {
+        BSTSearch(Root1, person, &found, &LocPtr);
+    }
+    else
+    {
+        BSTSearch(Root2, person, &found, &LocPtr);
+    }
+    if(found)
+    {
+        if(person.age==LocPtr->Data.age)
+        {
+            printf("The person with AMKA %d, AFM %d and age %d is in the catalogue\n",LocPtr->Data.AMKA,LocPtr->Data.afm,LocPtr->Data.age);
+        }
+        else{
+            printf("The person with AMKA %d has age %d different from the person you are looking for\n",LocPtr->Data.AMKA,LocPtr->Data.age);
+        }
+    }
+
+    else{
+        printf("The person with AMKA %d and age %d is Unknown\n",person.AMKA,person.age);
+    }
+    }
+
+    
+    return 0;
+}
+
+// Υλοποίηση συναρτήσεων
+void CreateBST(BinTreePointer *Root)
+/* Λειτουργία: Δημιουργεί ένα κενό ΔΔΑ.
+   Επιστρέφει:  Τον μηδενικό δείκτη(NULL) Root
+*/
+{
+    *Root = NULL;
+}
+
+boolean EmptyBST(BinTreePointer Root)
+/* Δέχεται:   Ενα ΔΔα με το Root να δείχνει στη ρίζα του.
+  Λειτουργία: Ελέγχει αν το ΔΔΑ είναι κενό.
+  Επιστρέφει: TRUE αν το ΔΔΑ είναι κενό και FALSE διαφορετικά
+*/
+{   return (Root==NULL);
+}
+
+void BSTInsert(BinTreePointer *Root, BinTreeElementType Item)
+/* Δέχεται:     Ένα ΔΔΑ με το δείκτη Root να δείχνει στη ρίζα του και ένα στοιχείο Item.
+   Λειτουργία:  Εισάγει το στοιχείο Item στο ΔΔΑ.
+   Επιστρέφει: Το τροποποιημένο ΔΔΑ με τον δείκτη Root να δείχνει στη ρίζα του
+*/
+{
+    BinTreePointer LocPtr, Parent;
+    boolean Found;
+
+    LocPtr = *Root;
+    Parent = NULL;
+    Found = FALSE;
+    while (!Found && LocPtr != NULL) {
+        Parent = LocPtr;
+        if (Item.AMKA < LocPtr->Data.AMKA)
+            LocPtr = LocPtr ->LChild;
+        else if (Item.AMKA > LocPtr ->Data.AMKA)
+            LocPtr = LocPtr ->RChild;
+        else
+            Found = TRUE;
+    }
+    if (Found)
+        printf("To %d EINAI HDH STO DDA\n", Item.AMKA);
+    else {
+        LocPtr = (BinTreePointer)malloc(sizeof (struct BinTreeNode));
+        LocPtr ->Data = Item;
+        LocPtr ->LChild = NULL;
+        LocPtr ->RChild = NULL;
+        if (Parent == NULL)
+            *Root = LocPtr;
+        else if (Item.AMKA < Parent ->Data.AMKA)
+            Parent ->LChild = LocPtr;
+        else
+            Parent ->RChild = LocPtr;
+    }
+}
+
+
+void BSTSearch(BinTreePointer Root, BinTreeElementType KeyValue, boolean *Found, BinTreePointer *LocPtr)
+/* Δέχεται:    Ένα ΔΔΑ με το δείκτη Root να δείχνει στη ρίζα του και μια τιμή KeyValue.
+   Λειτουργία: Αναζητά στο ΔΔΑ έναν κόμβο με τιμή KeyValue στο πεδίο κλειδί του.
+   Επιστρέφει: Η Found έχει τιμή TRUE και ο δείκτης LocPtr δείχνει στον κόμβο που
+                περιέχει την τιμή KeyValue, αν η αναζήτηση είναι επιτυχής.
+                Διαφορετικά η Found έχει τιμή FALSE
+*/
+{
+    boolean stop;
+
+    *LocPtr = Root;
+    stop = FALSE;
+    while (!stop && *LocPtr != NULL)
+    {
+        if (KeyValue.AMKA < (*LocPtr)->Data.AMKA)
+            *LocPtr = (*LocPtr)->LChild;
+        else
+            if (KeyValue.AMKA > (*LocPtr)->Data.AMKA)
+                *LocPtr = (*LocPtr)->RChild;
+            else stop = TRUE;
+    }
+   *Found=stop;
+}
+
+void InorderTraversal(BinTreePointer Root)
+/* Δέχεται:    Ένα δυαδικό δέντρο με το δείκτη Root να δείχνει στην ρίζα του.
+   Λειτουργία: Εκτελεί ενδοδιατεταγμένη διάσχιση του δυαδικού δέντρου και
+                επεξεργάζεται κάθε κόμβο ακριβώς μια φορά.
+   Εμφανίζει: Το περιεχόμενο του κόμβου, και εξαρτάται από το είδος της επεξεργασίας
+*/
+{
+    if (Root!=NULL) {
+        InorderTraversal(Root->LChild);
+        printf("(%d, %d, %d)",Root->Data.AMKA,Root->Data.afm,Root->Data.age);
+        InorderTraversal(Root->RChild);
+    }
+}
+
