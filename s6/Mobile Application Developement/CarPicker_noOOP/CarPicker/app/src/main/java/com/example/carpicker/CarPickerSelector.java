27a5bd57351a58c38a9f284e1f9610b03122d3f3
Efstathios Iosifidis <iefstathios@gmail.com>
2023-03-20 20:53:55 +0200

    CarPicker no OOP

---
+++ s6/Mobile Application Developement/CarPicker_noOOP/CarPicker/app/src/main/java/com/example/carpicker/CarPickerSelector.java
@@ -0,0 +1,30 @@
+package com.example.carpicker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarPickerSelector {
+
+    // Λίστα που παίρνει μια μάρκα (brand) και επιστρέφει μια λίστα από μοντέλα.
+    List<String> getCars(String brand){
+        List<String> models = new ArrayList<String>();
+
+        // Αν η μάρκα είναι Toyota, επέστρεψε
+        if (brand.equals("Toyota")){
+            models.add("Yaris");
+            models.add("Auris");
+        }
+
+        // Αν η μάρκα είναι VW, επέστρεψε
+        if (brand.equals("VW")){
+            models.add("Golf");
+            models.add("Polo");
+        }
+
+        // Αν η μάρκα είναι Nissan, επέστρεψε τιποτα
+
+        // Επιστρέφω τα μοντέλα
+        return models;
+    }
+
+}
